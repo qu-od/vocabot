6738a8fc9ea9e88eccb75dbe8e245b681581e270
Renegatto <46404781+Renegatto@users.noreply.github.com>
2020-04-21 04:36:03 +0300

    is_user_allowed fn refactored

--- _users_admission.py
+++ _users_admission.py
@@ -1,16 +1,16 @@
 
 
-def is_user_allowed(user):
-    ans = False #презумпция недопуска 
-    with open('assigned_bot_users.txt', 'rb') as F: 
-        for line in F:
-            s = line.decode('utf-8')
-            if (s.startswith("#") or s.startswith(' ')) == False:
-                s = s.replace('\r','')
-                s = s.replace('\n','')
-                if user == s:
-                    ans = True
-                    return ans
-    return ans
+def is_user_allowed(user: str) -> bool:
+    with open('assigned_bot_users.txt', 'rb') as file: 
+        decoded = map(lambda line: line.decode('utf-8'), file)
+        filtered = filter(lambda line: not (line.startswith("#") or 
+                                            line.startswith(' ')), 
+                          decoded)
+        withut_specsymbols = map(lambda line: line.replace('\r','')
+                                                  .replace('\n',''))
+        
+        if filter(lambda line: line == user): return True
+    
+    return False
 
 #далее функции для разрешения  на отдельные комманды
