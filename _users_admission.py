130918237993722797d3ba0e0769070046347c82
Animated-Machine <55834304+Animated-Machine@users.noreply.github.com>
2020-04-25 10:37:29 +0300

    user_adm lambda quarantine

--- _users_admission.py
+++ _users_admission.py
@@ -1,16 +1,16 @@
 
 
-def is_user_allowed(user: str) -> bool:
-    with open('assigned_bot_users.txt', 'rb') as file: 
-        decoded = map(lambda line: line.decode('utf-8'), file)
-        filtered = filter(lambda line: not (line.startswith("#") or 
-                                            line.startswith(' ')), 
-                          decoded)
-        without_specsymbols = map(lambda line: line.replace('\r','')
-                                                  .replace('\n',''))
-        
-        if list(filter(lambda line: line == user)): return True #if no such users with this name found, list will be empty
-    
-    return False
+def is_user_allowed(user):
+    ans = False #презумпция недопуска 
+    with open('assigned_bot_users.txt', 'rb') as F: 
+        for line in F:
+            s = line.decode('utf-8')
+            if (s.startswith("#") or s.startswith(' ')) == False:
+                s = s.replace('\r','')
+                s = s.replace('\n','')
+                if user == s:
+                    ans = True
+                    return ans
+    return ans
 
 #далее функции для разрешения  на отдельные комманды
